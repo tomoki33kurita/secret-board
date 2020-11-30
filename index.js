12f2f8089737b5d893a662ab341a2bb9b1b03697
tomoki33kurita <tomoki33kurita@gmail.com>
2020-11-30 23:18:30 +0900

    Implemented router func

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict'
 const http = require('http')
+const router = require('./lib/router')
 
 const server = http
-  .createServer((req, res) => res.end('hi'))
+  .createServer((req, res) => {
+    router.route(req,res)
+  })
   .on('error', e => console.error('Server Error', e))
   .on('clientError', e => console.error('Client Error', e))
 
